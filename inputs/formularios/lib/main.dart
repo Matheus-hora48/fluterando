d246b107068f6ae07f39214bb26de0db0fa77b26
Matheus Hora <matheus11hora@gmail.com>
2022-08-06 16:38:55 -0300

    nOVOS

--- inputs/formularios/lib/main.dart
+++ inputs/formularios/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:string_validator/string_validator.dart' as validator;
 
 void main() {
-  runApp(MaterialApp(
-    home: const MyApp(),
+  runApp(const MaterialApp(
+    home: MyApp(),
   ));
 }
 
@@ -14,7 +15,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final controller = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,24 +23,77 @@ class _MyAppState extends State<MyApp> {
       appBar: AppBar(
         title: const Text('Formulario'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: const[
-            CustomTextField(label: 'Name', icon: Icons.person,),
-            SizedBox(height: 16,),
-            CustomTextField(label: 'Email', icon: Icons.mail,),
-            SizedBox(height: 16,),
-            CustomTextField(label: 'Passoword', icon: Icons.vpn_key,),
-            SizedBox(height: 16,),
-            CustomTextField(label: 'Confirm Passoword', icon: Icons.vpn_key, hint: 'confirm passoword...',),
-            SizedBox(height: 32,)
-            // ElevatedButton.icon(
-            //   onPressed: () {}, 
-            //   icon: Icon(Icons.save), 
-            //   label: Text('Salvar'),
-            // ),
-          ],
+      body: Form(
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              const CustomTextField(
+                label: 'Name',
+                icon: Icons.person,
+                validator: (text) => text == null || text.isEmpty
+                    ? 'This field cannot be null'
+                    : null,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const CustomTextField(
+                label: 'Email',
+                icon: Icons.mail,
+                validator: (text){
+                  if(text == null || text.isEmpty)
+                },
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const CustomTextField(
+                label: 'Passoword',
+                icon: Icons.vpn_key,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const CustomTextField(
+                label: 'Confirm Passoword',
+                icon: Icons.vpn_key,
+                hint: 'confirm passoword...',
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      formKey.currentState!.save();
+                    }
+                  },
+                  icon: const Icon(Icons.save),
+                  label: const Text('Save'),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(primary: Colors.redAccent),
+                  onPressed: () {
+                    formKey.currentState?.reset();
+                  },
+                  icon: const Icon(Icons.cancel),
+                  label: const Text('Reset'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -50,15 +104,30 @@ class CustomTextField extends StatelessWidget {
   final String label;
   final String? hint;
   final IconData? icon;
+  final String? Function(String? text)? validator;
+  final void Function(String? text)? onSaved;
 
-  const CustomTextField({Key? key, required this.label, this.icon, this.hint}) : super(key: key);
+  const CustomTextField(
+      {Key? key,
+      required this.label,
+      this.icon,
+      this.hint,
+      this.validator,
+      this.onSaved})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      validator: (text) {
+        if (text == null || text.isEmpty) {
+          return 'This field cannot be null';
+        }
+      },
+      onSaved: (text) {},
       decoration: InputDecoration(
         labelText: label,
-        hintText: hint == null ? 'Type your $label...' : hint ,
+        hintText: hint ?? 'Type your $label...',
         border: const OutlineInputBorder(),
         prefixIcon: icon == null ? null : Icon(icon),
       ),
