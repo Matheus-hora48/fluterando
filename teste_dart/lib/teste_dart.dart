8f4cd7a42daf260ad59df4890c0009b6afac9027
Matheus-hora48 <matheus11hora@gmail.com>
2022-08-09 16:18:21 -0300

    Novos

--- teste_dart/lib/teste_dart.dart
+++ teste_dart/lib/teste_dart.dart
@@ -1,3 +1,23 @@
-int calculate() {
-  return 6 * 7;
+import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
+import 'dart:io';
+
+import 'package:uno/uno.dart';
+
+var url = 'https://jsonplaceholder.typicode.com/todos';
+
+void main() async {
+  final uno = Uno();
+  final response = await uno(url: url, method: 'get', );
+  print(response.data[3]);
+}
+
+Future<void> dioPackage() async {
+  final response = await Dio().get(url);
+  print(response.data[0]);
+}
+
+Future<void> httpPackage() async {
+  final response = await http.get(Uri.parse(url));
+  print(response.body);
 }
